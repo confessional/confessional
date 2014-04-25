184676e724d3215fa7dadc453fe1618bd929e2ce
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-04-25 12:26:22 +0200

    corpus maker script WIP

---
+++ corpus.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+include FileUtils
+
+class Corpus
+
+  def initialize()
+    @association_file = 'associations.txt'
+    @questions_file = 'questions.txt'
+    @sins_file = 'sins.txt'
+
+    @associations = File.open(@association_file).readlines.map { |l|
+      ids=l.split.map { |i| i.to_i }
+      [ids.shift, ids]
+    }
+    @sins = File.open(@sins_file).readlines.map { |l|
+      parts=l.split
+      [parts.shift.to_i, parts.join(' ').capitalize]
+    }
+    @questions = File.open(@questions_file).readlines.map { |l|
+      parts=l.split
+      [parts.shift.to_i, parts.join(' ').capitalize]
+    }
+  end
+
+
+  def question_by_id(question_id)
+    index = @questions.index { |x| x[0] == question_id }
+    @questions[index][1] unless index.nil?
+  end
+
+  def sin_question_ids(sin_id)
+    index=@associations.index { |x| x[0] == sin_id }
+    @associations[index][1] unless index.nil?
+  end
+
+  def run()
+    puts "#{@sins.count} sins, #{@questions.count} questions"
+
+    @sins.each { |s|
+      puts "-------------------------------------------------"
+      id=s[0]
+      sin=s[1]
+      puts "#{id}. #{sin}"
+      q_ids=sin_question_ids(id)
+      q_ids.each { |q_id|
+        puts "- #{q_id} #{question_by_id(q_id)} YES!"
+      } unless q_ids.nil?
+    }
+  end
+end
+
+Corpus.new().run()
