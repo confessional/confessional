2e36fb27986eebc9f301772d1eb4cc0764362757
Jacques <jba@zengularity.com>
2014-04-25 13:28:25 +0200

    Add the Tree builder

--- server/app/models/models.scala
+++ server/app/models/models.scala
@@ -1,4 +1,4 @@
-
+package models
 
 case class Question(id: Integer, text: String)
 
@@ -13,8 +13,31 @@ case object NC extends PossibleAnswer
 
 case class Answer(question: Question, sin: Sin, answer: PossibleAnswer)
 
-trait TreeElement
+sealed trait TreeElement
 
-case class TreeNode(question: Question, next: Map[PossibleAnswer, TreeElement]) extends TreeElement
-case class TreeLeaf(sin: Sin) extends TreeElement
-case class TreeFail extends TreeElement
+case class  TreeNode(uuid: String, question: Question, next: Map[PossibleAnswer, TreeElement]) extends TreeElement
+case class  TreeLeaf(sin: Sin) extends TreeElement
+case object TreeFail extends TreeElement
+
+object TreeNode {
+  def apply(question: Question, next: Map[PossibleAnswer, TreeElement]) = {
+    new TreeNode(java.util.UUID.randomUUID.toString, question, next)
+  }
+
+  def find(root: TreeNode, uuid: String): Option[TreeNode] = {
+    root.next.foldLeft[Option[TreeNode]](None){
+      case (None, (_, tn:TreeNode)) => if( tn.uuid == uuid ) Some(tn) else find(tn, uuid)
+      case (None, _)     => None
+      case (Some(tn), _) => Some(tn)
+    }
+  }
+
+  def next(root: TreeNode, uuid: String, pa: PossibleAnswer) = {
+    val te = for {
+      tn <- find(root, uuid)
+      te <- tn.next.get(pa)
+    } yield te
+
+    te.getOrElse(TreeFail)
+  }
+}
